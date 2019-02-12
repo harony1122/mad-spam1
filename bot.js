40f266c3ed250fd4217896fc528fa61a0a58492c
harony1122 <45592826+harony1122@users.noreply.github.com>
2019-02-12 15:27:08 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const client2 = new Discord.Client();
+
+client.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`Credit Farmm - Script By : Kahrbaa `);
+   console.log(`----------------`);
+   console.log(`Loadinng`);
+   console.log(`Loadinng.`);
+   console.log(`Loadinng..`);
+   console.log(`Loadinng...`);
+   console.log(`This Bots Online ' `);
+   console.log(`----------------`);
+});
+
+
+
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("544368853568716809")
+setInterval(function() {
+channel.send(`spaaamamaa`);
+}, 30)
+})
+
+
+
+
+
+client.login(process.env.TOKEN);// لا تغير فيها شيء
+client2.login(process.env.TOKEN2);// لا تغير فيها شيء
